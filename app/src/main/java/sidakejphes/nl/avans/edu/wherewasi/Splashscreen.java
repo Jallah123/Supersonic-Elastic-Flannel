1179d045343f9a56bd768942921589614dab35e1
Jelle van Es <jellevanes@hotmail.com>
2015-04-07 15:25:04 +0200

    added splashscreen. renamed dutch strings

---
+++ app/src/main/java/sidakejphes/nl/avans/edu/wherewasi/Splashscreen.java
@@ -0,0 +1,29 @@
+package sidakejphes.nl.avans.edu.wherewasi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import sidakejphes.nl.avans.edu.wherewasi.R;
+
+public class Splashscreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen_layout);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splashscreen.this, MainActivity.class);
+                Splashscreen.this.startActivity(mainIntent);
+                Splashscreen.this.finish();
+            }
+        }, 4000);
+    }
+}
